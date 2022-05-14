d8367f57e0f7a84da3de6ed20723f9420a1f57f5
Lucho-lja <ljallauca.a@gmail.com>
2022-05-14 12:44:51 -0500

    Add _feed view and Resolve problems abput sign up and create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,8 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order("created_at DESC")
+    @event = Event.new
   end
 
   # GET /events/1 or /events/1.json
