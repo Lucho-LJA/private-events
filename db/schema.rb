2a30b304dfb8a5646a5500056ef400152dc95c65
Lucho-lja <ljallauca.a@gmail.com>
2022-05-13 23:30:14 -0500

    Edit user_id to creator id into Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_14_040356) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_14_042620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_14_040356) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.bigint "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_14_040356) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "creator_id"
 end
