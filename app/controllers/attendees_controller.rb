3279928369eb5bea467ed5e0df5594dc6d540d90
Lucho-lja <ljallauca.a@gmail.com>
2022-05-14 21:59:55 -0500

    Add validation to guest and user

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,14 +1,27 @@
 class AttendeesController < ApplicationController
     def create
-        @attendee = current_user.attendees.build(attendee_event_id:params[:send_object])
-        respond_to do |format|
-            if @attendee.save
-              format.html { redirect_to attendee_url(@attendee), notice: "Event was successfully created." }
-            else
-              format.html { render "events/index", status: :unprocessable_entity }
-            end
+        if current_user
+            @attendee = current_user.attendees.build(attendee_event_id:params[:send_object])
+            respond_to do |format|
+                if @attendee.save
+                format.html { redirect_to event_url(Event.find(params[:send_object])), 
+                    notice: "Event was successfully created." }
+                else
+                format.html { render "events/index", status: :unprocessable_entity }
+                end
+            end    
+        else
+            redirect_to new_user_session_url
         end
+    end
 
+    def destroy
+        @attendee = current_user.attendees.where(attendee_event_id:params[:send_object])
+        @attendee.destroy_all
+        respond_to do |format|
+          format.html { redirect_to events_url(Event.find(params[:send_object])), 
+            notice: "Event was successfully destroyed." }
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :attendees
+  resources :attendees, only: [:create, :destroy, :show]
   # Defines the root path route ("/")
   # root "articles#index"
   root "events#index"
