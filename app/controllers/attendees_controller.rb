fda62f2869e19d7496426e2398d52702a24c099a
Lucho-lja <ljallauca.a@gmail.com>
2022-05-14 20:10:56 -0500

    Add join buttom functionality

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,2 +1,14 @@
 class AttendeesController < ApplicationController
+    def create
+        @attendee = current_user.attendees.build(attendee_event_id:params[:send_object])
+        respond_to do |format|
+            if @attendee.save
+              format.html { redirect_to attendee_url(@attendee), notice: "Event was successfully created." }
+            else
+              format.html { render "events/index", status: :unprocessable_entity }
+            end
+        end
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :attendees
   # Defines the root path route ("/")
   # root "articles#index"
   root "events#index"
